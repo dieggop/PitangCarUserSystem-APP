3cf1e10663f78cd1e13abb7c9a9c235d0680a0e5
dieggopsc@gmail.com <Asenhaeminha31@>
2020-01-19 23:57:50 -0300

    LIstagem de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosListagemComponent } from './usuarios-listagem/usuarios-listagem.component';
@@ -21,6 +21,7 @@ import { UsuariosService } from './usuarios.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [UsuariosService],
--- src/app/usuarios.service.ts
+++ src/app/usuarios.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor() { }
+  constatosUrl = 'https://pitangcarusersystem.herokuapp.com/api/users';
+
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<any[]>(`${this.constatosUrl}`)
+  }
 }
